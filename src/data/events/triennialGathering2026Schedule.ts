0f04251917b483e1ca080546a35331527949b043
poncardasm <159589562+poncardasm@users.noreply.github.com>
2025-12-07 01:14:06 +0200

    feat: add schedule details for the Triennial Gathering 2026

--- src/data/events/triennialGathering2026Schedule.ts
+++ src/data/events/triennialGathering2026Schedule.ts
@@ -40,6 +40,66 @@ export const triennialGathering2026Schedule = [
         speaker: '',
         details: '',
       },
+      {
+        time: '10:30 – 11:00',
+        title:
+          'Equity vs Equality: Amplifying your presence in nursing leadership in the Nordic region',
+        speaker: 'Wendill Viejo',
+        details:
+          'Board Member, Félag Íslenska Hjúkrunarfræðinga (Icelandic Nurses Union)',
+      },
+      {
+        time: '11:00 – 12:30',
+        title: 'Lunch Break',
+        speaker: '',
+        details: '',
+      },
+      {
+        time: '12:30 – 14:00',
+        title:
+          'Breakout session: Moving to Nordic Countries: Filipino perspectives and collective experiences',
+        speaker: 'Dr. Unnur Dís Skaptadóttir (Facilitator)',
+        details:
+          'University of Iceland. Delegates to be grouped by country to discuss and present results.',
+      },
+      {
+        time: '14:00 – 14:15',
+        title: 'Short Interactive Quiz (with prizes)',
+        speaker: '',
+        details: '',
+      },
+      {
+        time: '14:15 – 14:45',
+        title: 'When Cultures Collide: Relationships',
+        speaker: 'Ragnheiður Bjartman Eiríksdóttir',
+        details:
+          'Reproductive Health Specialist (sex therapist), mental health nurse, Author, Knitting Designer',
+      },
+      {
+        time: '14:45 – 15:00',
+        title: 'Coffee Break',
+        speaker: '',
+        details: '',
+      },
+      {
+        time: '15:00 – 15:30',
+        title: 'Workshop: Crafting our Message to the Filipino government',
+        speaker: 'FINAN officers (Facilitators)',
+        details:
+          '"In a perfect world, how can the Filipino government make ours and our families\' lives better?"\n\nEach delegation will then be responsible for formally delivering this message to the Filipino embassies and consulates in their respective countries.\n\nThe message will be formally submitted to Filipino embassies, consulates, Philippine Congress, or Senate.',
+      },
+      {
+        time: '15:30 – 16:00',
+        title: 'Closing Remarks and turnover to the next country host',
+        speaker: '',
+        details: '',
+      },
+      {
+        time: '19:00 – 21:00',
+        title: 'Gala Night & Fellowship',
+        speaker: '',
+        details: '',
+      },
     ],
   },
   {
